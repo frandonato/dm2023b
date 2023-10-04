7f27203e40c640c43016856f67bd30ede9d5fb9e
frandonato <frandonato2014@gmail.com>
2023-10-04 23:58:11 +0000

    commit

--- src/workflow-inicial/Exp A6/641_TS_training_strategy_04.r
+++ src/workflow-inicial/Exp A6/641_TS_training_strategy_04.r
@@ -19,20 +19,16 @@ PARAM$exp_input <- "FE6310"
 # entreno en 18 meses
 
 PARAM$future <- c(202109)
-PARAM$final_train <- c(
-  202107, 202106, 202105, 202104, 202103, 202102,
+PARAM$final_train <- c(202106, 202105, 202104, 202103, 202102,
   202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912,
-  201911, 201910, 201909
+  201911, 201910, 201909,201908
 )
 
-PARAM$train$training <- c(
-  202105, 202104, 202103, 202102, 202101,
-  202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911,
-  201910, 201909, 201908, 201907
-)
+PARAM$train$training <- c(202101,202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911,
+  201910, 201909, 201908, 201907,201906,201905,201904,201903)
 
-PARAM$train$validation <- c(202106)
-PARAM$train$testing <- c(202107)
+PARAM$train$validation <- c(202102,202104)
+PARAM$train$testing <- c(202103,202105,20216)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
