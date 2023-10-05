7ca1311f9067dac0606d8567db1bd289d9040d16
frandonato <frandonato2014@gmail.com>
2023-10-05 00:12:18 +0000

    commit

--- src/workflow-inicial/601_RUN_correr_workflow_04.r
+++ src/workflow-inicial/601_RUN_correr_workflow_04.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-inicial/z611_CA_reparar_dataset.r")
-source("~/dm2023b/src/workflow-inicial/z621_DR_corregir_drifting.r")
-source("~/dm2023b/src/workflow-inicial/z631_FE_historia.r")
-source("~/dm2023b/src/workflow-inicial/z641_TS_training_strategy.r")
+source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_04.r")
+source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_04.r")
+source("~/dm2023b/src/workflow-inicial/631_FE_historia_04.r")
+source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_04.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-inicial/z651_HT_lightgbm.r")
-source("~/dm2023b/src/workflow-inicial/z661_ZZ_final.r")
+source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_04.r")
+source("~/dm2023b/src/workflow-inicial/661_ZZ_final_04.r")
